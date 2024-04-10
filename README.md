40d70ee8196c99198b96b5a8d13963676687df8d
Dmytro Shtrikker <shtrikker28@gmail.com>
2024-04-10 14:27:13 +0300

    add clang-format
    Signed-off-by: Dmytro Shtrikker <shtrikker28@gmail.com>

--- README.md
+++ README.md
@@ -21,3 +21,19 @@ cmake -B build
 cd build
 make
 ```
+
+# Scripts/Tools
+
+## clang-format
+From root of project, run
+```bash
+./scripts/run_clang_format.sh
+```
+to format all project sources with project's code style
+
+---
+From root of project, run
+```bash
+./scripts/install_hooks.sh
+```
+to force git to apply project's code style on pre-commit
---
+++ scripts/run_clang_format.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+DISTRO=''
+
+if [ -f "/etc/debian_version" ]; then
+  DISTRO='debian'
+fi
+
+if [ -x "$(command -v clang-format-16)" ]; then
+  find src -iname '*.h' -o -iname '*.c' -o -iname '*.cpp' | xargs clang-format-16 -i
+else
+  echo "clang-format-16 is not installed"
+  case $DISTRO in
+    debian)
+      echo "Try 'sudo apt install clang-format-16'"
+      ;;
+  esac
+fi
